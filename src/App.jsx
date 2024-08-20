abf5867e3821ade0b39ad3a7fe324ccd77deb63a
kianoushit1999 <kianoushit1999@gmail.com>
2024-08-20 17:21:31 +0330

    adding Routes to the project but has some issues regarding its style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Bookings from "./pages/Bookings";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
 
+function App() {
   return (
     <>
-      Hello
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
